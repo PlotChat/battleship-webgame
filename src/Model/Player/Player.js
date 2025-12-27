27522b3a6c9f0097df00b76374cacfd2deaa1979
PlotChat <levietanh1234567@gmail.com>
2025-12-27 19:42:28 +0700

    Add controller and damage to Player class

--- src/Model/Player/Player.js
+++ src/Model/Player/Player.js
@@ -1,17 +1,27 @@
 import { validateName, validateNumber, validateType } from "../../Utils/validateInput";
-import { Ship } from "../Ship";
+import { ArenaController } from '../../Controller/index';
+import { Ship, Arena } from "../../Model/index";
 
 export class Player {
 	#name;
 	#points;
 	#ships;
 	#turns;
+	#damage;
+	#controller = null;
 
-	constructor({ name = "Player", points = 0, ships = [], turns = 0 } = {}) {
+	constructor({ name = "Player", points = 0, ships = [], turns = 0, damage = 1 } = {}) {
 		this.name = name;
 		this.points = points;
 		this.ships = ships;
 		this.turns = turns;
+		this.damage = damage;
+	}
+
+	attackArena(blockLoc, target){
+		validateType(target, "Target arena", ArenaController);
+
+		target.receiveAttack(blockLoc, this.#damage);
 	}
 
 	set name(username) {
@@ -27,9 +37,10 @@ export class Player {
 
 	set ships(arr) {
 		validateType(arr, "Ship list", Array);
-		arr.find(ship => {
+		arr.forEach(ship => {
 			validateType(ship, "Ship", Ship);
 		})
+
 		this.#ships = arr;
 	}
 
@@ -37,6 +48,18 @@ export class Player {
 		validateNumber(totalTurns, "Player turns", "Integer");
 		if (totalTurns < 0) throw new Error("Total turns must be > 0");
 		this.#turns = totalTurns;
+	}
+
+	set controller(obj) {
+		validateType(obj, "Object", Arena);
+		this.#controller = obj;
+	}
+
+	set damage(value){
+		validateNumber(value, "Damage value", "Integer");
+		if(value < 0) value = 0;
+
+		this.#damage = value;
 	}
 
 	get name() {
@@ -50,5 +73,11 @@ export class Player {
 	}
 	get turns() {
 		return this.#turns;
+	}
+	get controller() {
+		return this.#controller;
+	}
+	get damage(){
+		return this.#damage;
 	}
 }
