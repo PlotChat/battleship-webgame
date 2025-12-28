8f55933555c9cd1f943fcd92b477e8d0ec104a5f
PlotChat <levietanh1234567@gmail.com>
2025-12-29 00:32:47 +0700

    Add functions to index.js to run game

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import {Carrier} from './Model/index'
+import { ArenaController, GameController } from './Controller';
+import { 
+    Arena,
+    Player,
+} from './Model/index'
 
 function initApp(){
-    let c = new Carrier();
-    console.log(c);
+    let p1Arena = new Arena({size: 7});
+    let p1ArenaController = new ArenaController({area: p1Arena});
+    let p1 = new Player({name: "p1", controller: p1ArenaController});
+
+    let game = new GameController({ players: [p1]});
+
+    setTimeout(() => game.startGame([p1]), 3000);
 }
 
 initApp();
