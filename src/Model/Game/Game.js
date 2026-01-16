88dc02ce256fd25d286cfb34637eb13cafe25b05
PlotChat <levietanh1234567@gmail.com>
2026-01-16 20:21:31 +0700

    Fix wrong max rounds calculation

--- src/Model/Game/Game.js
+++ src/Model/Game/Game.js
@@ -1,45 +1,79 @@
-import { validateType } from '../../Utils/index';
-import { 
-    Player,
-    GameSettings
-} from '../../Model/index';
-import { ArenaController } from '../../Controller/index';
+import { validateType } from "../../Utils/index";
+import { Player, GameSettings } from "../../Model/index";
+import { ArenaController } from "../../Controller/index";
 
 export class Game {
-    #players;
-    #currentPlayer;
-    #currentArenaController;
+	#players;
+	#currentPlayer;
+	#currentArenaController;
+	#currentRound;
+	#maxRounds;
 
-    constructor({players = [], currentArenaController = null, currentPlayer} = {}){
-        this.players = players;
-        this.currentArenaController = currentArenaController;
-        this.currentPlayer = players[0];
-    }
-
-    set players(playerList) {
-        validateType(playerList, "Player list" , Array);
-
-        if (!(playerList.length >= 0 && playerList.length <= GameSettings.maxPlayers)) 
-            throw new Error(`There must be 0 or <= ${GameSettings.maxPlayers} players`);
+	constructor({
+		players = [],
+		currentArenaController = null,
+		currentPlayer,
+		maxRounds = null,
+		currentRound = null,
+	} = {}) {
+		this.players = players;
+		this.currentArenaController = currentArenaController;
+		this.currentPlayer = currentPlayer;
         
-        playerList.forEach(player => {
-            validateType(player, "Player", Player);
-        });
-        
-        this.#players = playerList;
-    }
+        if(!(maxRounds || currentRound)){
+            this.#maxRounds = Math.pow(this.#currentArenaController.arena.size, 2);
+            this.#currentRound = 1;
+        }
+	}
 
-    set currentPlayer(player) {
-        validateType(player, "Player", Player);
-        this.#currentPlayer = player;
-    }
+	set players(playerList) {
+		validateType(playerList, "Player list", Array);
 
-    set currentArenaController(controller) {
-        validateType(controller, "Arena controller", ArenaController);
-        this.#currentArenaController = controller;
-    }
+		if (
+			!(playerList.length >= 0 && playerList.length <= GameSettings.maxPlayers)
+		)
+			throw new Error(
+				`There must be 0 or <= ${GameSettings.maxPlayers} players`
+			);
 
-    get players(){ return this.#players };
-    get currentPlayer() { return this.#currentPlayer };
-    get currentArenaController() { return this.#currentArenaController };
+		playerList.forEach((player) => {
+			validateType(player, "Player", Player);
+		});
+
+		this.#players = playerList;
+	}
+
+	set currentPlayer(player) {
+		validateType(player, "Player", Player);
+		this.#currentPlayer = player;
+	}
+
+	set currentArenaController(controller) {
+		validateType(controller, "Arena controller", ArenaController);
+		this.#currentArenaController = controller;
+	}
+
+	set maxRounds(rounds) {
+		this.#maxRounds = rounds;
+	}
+
+	set currentRound(round) {
+		this.#currentRound = round;
+	}
+
+	get players() {
+		return this.#players;
+	}
+	get currentPlayer() {
+		return this.#currentPlayer;
+	}
+	get currentArenaController() {
+		return this.#currentArenaController;
+	}
+	get maxRounds() {
+		return this.#maxRounds;
+	}
+	get currentRound() {
+		return this.#currentRound;
+	}
 }
